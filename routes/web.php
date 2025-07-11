d04b4f73a63517d3b45532d217206f69ed01a2fb
this-is-mirita <popap182@gmail.com>
2025-07-11 17:40:00 +0700

    test reset

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', [IndexController::class, 'index']);
 Route::post('/delete/{id}', [IndexController::class, 'destroy']);
 Route::get('/create', [IndexController::class, 'create']);
+Route::post('/create', [IndexController::class, 'store']);
+
+
 Route::post('/create', [IndexController::class, 'store']);
 
